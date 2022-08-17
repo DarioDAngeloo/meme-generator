a92bec15e002d653666fc697180430432ecd78f8
Dario DAngelo <dariodangelo21@gmail.com>
2022-08-17 09:39:44 -0300

    added text input showing to tue user and fix css

--- tsx/src/components/Form.tsx
+++ tsx/src/components/Form.tsx
@@ -27,19 +27,44 @@ const Form = () => {
     }));
   };
 
+  // listener to inputs texts
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  };
+
   // return section to DOM
 
   return (
     <main>
       <div className="form">
-        <input type="text" className="form--input" placeholder="Top text" />
-        <input type="text" className="form--input" placeholder="Bottom text" />
+        <input
+          type="text"
+          className="form--input"
+          name="topText"
+          placeholder="Top text"
+          value={memes.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form--input"
+          name="bottomText"
+          placeholder="Bottom text"
+          value={memes.bottomText}
+          onChange={handleChange}
+        />
         <button className="form--button" onClick={getMeme}>
           Get a new meme image
         </button>
       </div>
-      <div className="img-container">
+      <div className="meme">
         <img src={memes.randomImage} className="meme--image" alt="" />
+        <h2 className="meme--text top">{memes.topText}</h2>
+        <h2 className="meme--text bottom">{memes.bottomText}</h2>
       </div>
     </main>
   );
